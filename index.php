6ebc5ff13ebc3cb456869499cb22e5778ec02743
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2024-01-18 17:50:17 -0300

    Atualizado 18/01

--- index.php
+++ index.php
@@ -2,22 +2,10 @@
 
 session_start();
 
-if (!isset($_SESSION['tasks'])) {
 
+if (!isset($_SESSION['tasks'])) {
     $_SESSION['tasks'] = array();
 }
-
-if (isset($_GET['task_name'])) {
-
-    array_push($_SESSION['tasks'], $_GET['task_name']);
-    unset($_GET['task_name']);
-}
-
-if (isset($_GET['clear'])) {
-
-    unset($_SESSION['tasks']);
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -39,11 +27,24 @@ if (isset($_GET['clear'])) {
             <h1>Gerenciador de Tarefas</h1>
         </div>
         <div class="form">
-            <form action="" method="get">
+            <form action="task.php" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="insert" value="insert">
                 <label for="task_name">Tarefa:</label>
                 <input type="text" name="task_name" placeholder="Nome da Tarefa">
+                <label for="task_description">Descrição da tarefa: </label>
+                <input type="text" name="task_description" placeholder="Descrição da tarefa">
+                <label for="task_date">Data da tarefa</label>
+                <input type="date" name="task_date">
+                <label for="task_image">Imagem: </label>
+                <input type="file" name="task_image">
                 <button type="submit">Cadastrar</button>
             </form>
+            <?php
+            if (isset($_SESSION['message'])) {
+                echo "<p style='color: #e4e006';>" . $_SESSION['message'] . "</p>";
+                unset($_SESSION['message']);
+            }
+            ?>
         </div>
         <div class=" separator">
 
@@ -54,18 +55,27 @@ if (isset($_GET['clear'])) {
 
                 echo "<ul>";
                 foreach ($_SESSION['tasks'] as $key => $task) {
-                    echo "<li>$task</li>";
+                    echo "<li>
+                            <a href='details.php?key=$key'>" . $task['task_name'] . "</a>
+                            <button type='button' class='btn-clear' onclick='deletar$key()'>Remover</button>
+                            <script>
+                                function deletar$key(){
+                                    if ( confirm('Confirmar remoção?') ) {
+                                        window.location = 'http://localhost:8000/task.php?key=$key';
+                                    }
+                                    return false;
+                                }
+                            </script>
+                        </li>";
                 }
+
                 echo "<ul>";
             }
 
 
             ?>
 
-            <form action="" method="get">
-                <input type="hidden" name="clear" value="clear">
-                <button type="submit" class='btn-clear'>Limpar Tarefas</button>
-            </form>
+
         </div>
         <div class="footer">
             <p>Desenvolvido por Gustavo Motta/ Com uma ajudinha né kkkk</p>
